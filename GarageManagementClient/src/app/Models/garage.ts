00849c6eefb732b7948dc040bcb5e2405a389399
RutKoren <Ruth-koren@gmail.com>
2025-12-01 15:17:56 +0200

    Create garage class

---
+++ GarageManagementClient/src/app/Models/garage.ts
@@ -0,0 +1,18 @@
+export class Garage {
+
+_id: number = 0;
+mispar_mosah?: number;
+shem_mosah?: string;
+cod_sug_mosah?: number;
+sug_mosah?: string;
+ktovet?: string;
+yishuv?: string;
+telephone?: string;
+mikud?: number;
+cod_miktzoa?: number;
+miktzoa?: string;
+menahel_miktzoa?: string;
+rasham_havarot?: number;
+TESTIME?: string;
+}
+
